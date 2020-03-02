2c020deefd6056ca457dc859e057a3f8b1b3ce6d
Nadim Tuhin <nadimtuhin@gmail.com>
2020-03-02 11:41:08 +0600

    added leetcode test 25

---
+++ leetcode/leet-25.spec.js
@@ -0,0 +1,10 @@
+const { solution } = require('./leet-25');
+let l = sol = 10;
+
+it('should test ', function() {
+    expect(l).toEqual(sol);
+});
+
+it('should test ', function() {
+    expect(l).toEqual(sol);
+});
