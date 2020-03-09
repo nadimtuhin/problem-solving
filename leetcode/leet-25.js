c27a435ebdbdbbec834a49050ce4b6d4347aca0e
Nadim Tuhin <nadimtuhin@gmail.com>
2020-03-09 08:25:24 +0600

    solved problem 25 after a week :D

--- leetcode/leet-25.js
+++ leetcode/leet-25.js
@@ -1,5 +1,4 @@
 // Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
-
 // k is a positive integer and is less than or equal to the length of the linked list. 
 // If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
 
@@ -16,12 +15,103 @@
 // Only constant extra memory is allowed.
 // You may not alter the values in the list's nodes, only nodes itself may be changed.
 
-function Node(val, left, right) {
-    return { val, left, right };
+function serializeLinkedList(node) {
+    if (node.next) {
+        return node.val + "->" + serializeLinkedList(node.next);
+    }
+
+    if (!node.val) {
+        return null;
+    }
+
+    return node.val;
 }
 
-function solution(list) {
+function getLength(list) {
+    let node = list;
+    let length = 0;
 
+    while(node) {
+        length++;
+        node = node.next;
+    }
+
+    return length;
 }
 
-module.exports = { solution };
+function ListNode(val, next) {
+    this.val = val;
+    this.next = next;
+}
+
+ListNode.prototype.toString = function() {
+    return serializeLinkedList.call(null, this);
+};
+
+function arrayToLinkedList(listArr) {
+    let head = new ListNode(0);
+    let latestHead = head;
+
+    for(let ii=0; ii<listArr.length; ii++) {
+        latestHead.next = listArr[ii];
+        latestHead = latestHead.next;
+    }
+
+    return head.next;
+}
+
+function reverseLinkedList(arr) {
+    arr = arr.slice();
+
+    const reversedArr = arr.reverse();
+
+    arr[arr.length-1].next = arr[0].next;
+
+    const reversedLL = arrayToLinkedList(reversedArr);
+
+    return reversedLL;
+}
+
+function reverseKGroup(node, k) {
+    let length = getLength(node);
+    let head = new ListNode(0);
+    head.next = node;
+
+    let tempHeadII = head;
+    let tempHeadJJ = head;
+
+    for(let ii=0; ii<length; ii=ii+k) {
+        let tempArr = [];
+        for(let jj=0; jj<k; jj++) {
+            if(tempHeadJJ) {
+                tempHeadJJ = tempHeadJJ.next;
+            } else {
+                break;
+            }
+
+            if(tempHeadJJ) {
+                tempArr.push(tempHeadJJ);
+            } else {
+                break;
+            }
+        }
+
+        if(tempArr.length != k) break;
+
+        let ll = reverseLinkedList(tempArr);
+        tempHeadII.next = ll;
+
+        tempHeadII = tempArr[0];
+        tempHeadJJ = tempArr[0];
+    }
+
+    return head.next;
+}
+
+module.exports = { 
+    Node: ListNode, 
+    getLength, 
+    serializeLinkedList, 
+    solution: reverseKGroup,
+    reverseLinkedList
+};
--- leetcode/leet-25.spec.js
+++ leetcode/leet-25.spec.js
@@ -1,10 +1,37 @@
-const { solution } = require('./leet-25');
+const { solution, Node, serializeLinkedList, getLength } = require('./leet-25');
 let l = sol = 10;
 
-it('should test ', function() {
-    expect(l).toEqual(sol);
+it('should test 1 2 3 4 on 2', function() {
+    const node = new Node(1, new Node(2, new Node(3, new Node(4))));
+
+    expect(solution(node, 2).toString()).toEqual('2->1->4->3');
 });
 
-it('should test ', function() {
-    expect(l).toEqual(sol);
+it('should test 1 2 3 4 on 3', function() {
+    const node = new Node(1, new Node(2, new Node(3, new Node(4))));
+
+    expect(solution(node, 3).toString()).toEqual('3->2->1->4');
 });
+
+it('should test 1 2 3 4 5', function() {
+    const node = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+
+    expect(solution(node, 2).toString()).toEqual('2->1->4->3->5');
+});
+
+it('should test 1 2 3 4 5', function() {
+    const node = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+
+    expect(solution(node, 3).toString()).toEqual('3->2->1->4->5');
+});
+
+const node = new Node(1, new Node(2, new Node(3)));
+
+it('should test getLength', function() {
+    expect(getLength(node)).toEqual(3);
+});
+
+it('should serialize linkedList', () => {
+    expect(serializeLinkedList(node)).toEqual('1->2->3');
+})
+
